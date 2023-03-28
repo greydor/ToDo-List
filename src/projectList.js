f00d35b150ffd2e1f59c8b5fe752c2451eef623c
greydor <ghub@us.jabbermail.com>
2023-03-27 22:51:16 -0400

    Add ability to change and delete projects

--- src/projectList.js
+++ src/projectList.js
@@ -4,14 +4,15 @@ import task from "./task";
 const projectList = (() => {
     const task1 = task("test", "44", "1-2-23", "High");
     const task2 = task("test2", "N/A", "2-6-23", "Low");
+    const task3 = task("test3", "879", "2-2-23", "Low");
 
     const exampleTasks = taskList();
     exampleTasks.add(task1);
     exampleTasks.add(task2);
 
     const exampleTasks2 = taskList();
-    exampleTasks.add(task1);
-    exampleTasks.add(task2);
+    exampleTasks2.add(task3);
+    exampleTasks2.add(task3);
 
     const projects = [
         { title: "Default Project", tasks: exampleTasks },
--- src/renderProjects.js
+++ src/renderProjects.js
@@ -6,15 +6,41 @@ const defaultProjectEl = document.querySelector(".projects-item");
 function renderProjects() {
     projectList.get().forEach((project, index) => {
         const projectEl = defaultProjectEl.cloneNode(true);
-        projectEl.querySelector("input").value = project.title;
+        const projectNameEl = projectEl.querySelector("input")
+        projectNameEl.value = project.title;
         projectEl.setAttribute("data-index", index);
         projectsListEl.appendChild(projectEl);
 
+        const btnRenameAccept = projectEl.querySelector(".btn-project-rename-accept");
         const btnRename = projectEl.querySelector(".btn-project-rename");
-        btnRename.addEventListener("click", (e) => {
-            const editListEl = e.target.parentElement
-            editListEl.querySelector("input").classList.add("edit-project-name")
+        btnRename.addEventListener("click", () => {
+
+            projectNameEl.classList.add("edit-project-name")
+            projectNameEl.removeAttribute("readonly")
+            btnRename.style.display = "none"
+            btnRenameAccept.style.display = "inline"
         });
+
+        btnRenameAccept.addEventListener("click", () => {
+            // Update project name
+            const newProjectName = projectNameEl.value
+            const dataIndex = projectEl.getAttribute("data-index")
+            projectList.get()[dataIndex].title = newProjectName
+            
+            btnRename.style.display = "inline"
+            btnRenameAccept.style.display = "none"
+            projectNameEl.setAttribute("readonly", "")
+        })
+
+        const btnDelete = projectEl.querySelector(".btn-project-delete")
+        btnDelete.addEventListener("click", () => {
+            const dataIndex = projectEl.getAttribute("data-index")
+            projectList.remove(dataIndex)
+            hideProjects()
+            renderProjects()
+        })
+
+
     });
 }
 
